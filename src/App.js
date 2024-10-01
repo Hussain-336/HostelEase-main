84e802c216312945e8f795e0d77e1a19aa4cb93e
Hussain-336 <hussainullahk373@gmail.com>
2024-10-01 16:05:12 +0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Herosection from './pages/Herosection';
-import Hostels from './pages/Hostels';
-import Header from './components/Header';
+import Home from './pages/Home';
+import Cities from './pages/Cities';
+import Header from './layouts/Header';
 import Progress from './pages/Progress';
-import Footer from './components/Footer';
-
-
-
-
-
+import Footer from './layouts/Footer';
+import AllHostels from './services/AllHostels';
+import About from './components/About';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Studentform from './components/Studentform';
+import layouts from './layouts';
 
 function App() {
   return (
     <>
-    <Header/>
+            <Router>
+            <Routes>
+                <Route path="/" element={
+                 <layouts> <Home /></layouts>
+                }
+                   />
+                <Route path="/About" element={
+                 <layouts> <About /></layouts>
+                } 
+                />
+                <Route path="/Cities" element={
+                  <layouts><Cities /></layouts>
+                } 
+                  />
+                <Route path="/AllHostels" element={
+                 <layouts> <AllHostels /></layouts>
+              } />
+                <Route path="/Progress" element={
+                  <layouts><Progress /></layouts>
+              } />
+                <Route path="/Header" element={<Header />} />
+                <Route path="/Footer" element={<Footer />} />
+                <Route path="/Signup" element={
+                <Signup />
+                } />
+               <Route path="/Login" element={<Login />} />
+               <Route path="/Studentform" element={<Studentform />} />
+
+            </Routes>
+        </Router>
+
+
+    {/* <Header/>
     <Herosection/>
-    <Hostels/>
+    <Cities/>
+    <AllHostels/>
     <Progress/>
-    <Footer/>
+    <Footer/> */}
     
 
-    <div className="App">
-    </div>
+    {/* <div className="App">
+    </div> */}
 
     </>
       
